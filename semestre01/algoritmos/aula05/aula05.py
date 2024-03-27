a21d18eecb5692ff987affb8775c4512d9f45f73
Thales Souza Chagas <thales.souz@outlook.com>
2024-03-27 19:39:19 -0300

    aula 05 - python

---
+++ semestre01/algoritmos/aula05/aula05.py
@@ -0,0 +1,8 @@
+# Faça um programa que receba a altura e a largura de um retângulo e calcule a área do retângulo.
+
+height = int(input("Digite a altura do retângulo em cm: "))
+width = int(input("Digite a largura do retângulo em cm: "))
+
+area = height * width
+
+print("A área do retângulo é:", area, "cm²")
