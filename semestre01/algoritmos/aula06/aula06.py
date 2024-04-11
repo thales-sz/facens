01adccf9ec60935ace8806a5ffe9bbe6176ca603
Thales Souza Chagas <thales.souz@outlook.com>
2024-04-10 21:08:36 -0300

    aula 06

---
+++ semestre01/algoritmos/aula06/aula06.py
@@ -0,0 +1,26 @@
+grade1 = int(input('Digite a primeira nota:'))
+grade2 = int(input('Digite a segunda nota:'))
+
+average = (grade1 + grade2) / 2
+
+if average >= 7:
+  print('Aprovado')
+elif average < 7 and average >= 5:
+  print('Exame')
+else:
+  print('Reprovado')
+
+
+grade1 = int(input('Digite a primeira nota:'))
+grade2 = int(input('Digite a segunda nota:'))
+
+average = (grade1 + grade2) / 2
+
+
+
+if average >= 7:
+  print('Aprovado')
+elif average < 7 and average >= 5:
+  print('Exame')
+else:
+  print('Reprovado')
